038e679bbbdf94383d683507a090e5f2443be055
Johan Steven Bueno Rojas <johanstevenbr@ufps.edu.co>
2026-09-10 07:14:47 -0500

    Agregar interacción con un botón

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 const titulo = document.querySelector("h2");
 
 console.log(titulo);
-const total = document.querySelector("#totalUsuarios");
+
+const total = 
+    document.querySelector("#totalUsuarios");
 
 total.textContent = "200";
+
+const boton =
+    document.querySelector(
+        "#btnCargar"
+    );
+
+boton.addEventListener(
+    "click",
+    function () {
+
+        console.log(
+            "El usuario hizo clic"
+        );
+
+    }
+);
