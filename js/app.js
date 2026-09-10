1dad4c9e2e6138ca8e5eaf063ace1cd7ff5f24ca
Johan Steven Bueno Rojas <johanstevenbr@ufps.edu.co>
2026-09-10 07:31:14 -0500

    Mostrar los usuarios en la tabla

--- js/app.js
+++ js/app.js
@@ -32,3 +32,72 @@ fetch(
     console.log(data);
 
 });
+
+async function obtenerUsuarios() {
+
+    const respuesta =
+        await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+
+    const usuarios =
+        await respuesta.json();
+
+    console.log(
+        usuarios
+    );
+
+}
+
+async function obtenerUsuarios() {
+
+    const respuesta =
+        await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+
+    const usuarios =
+        await respuesta.json();
+
+    mostrarUsuarios(
+        usuarios
+    );
+
+}
+
+obtenerUsuarios();
+
+function mostrarUsuarios(
+    usuarios
+) {
+
+    const tabla =
+        document.querySelector(
+            "#tablaUsuarios"
+        );
+
+    tabla.innerHTML = "";
+
+    usuarios.forEach(
+        usuario => {
+
+            tabla.innerHTML += `
+                <tr>
+                    <td>
+                        ${usuario.name}
+                    </td>
+
+                    <td>
+                        ${usuario.email}
+                    </td>
+
+                    <td>
+                        ${usuario.address.city}
+                    </td>
+                </tr>
+            `;
+
+        }
+    );
+
+}
